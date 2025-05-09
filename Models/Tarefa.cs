5335d5533d2e8591f6c8a50a1566220f30f40784
Kynotec <2211867@my.ipleiria.pt>
2025-05-09 11:24:23 +0100

    create Tarefa

---
+++ Models/Tarefa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks.Models
+{
+    internal class Tarefa
+    {
+        private int Id { get; set; }
+        private int IdGestor { get; set; }
+
+        private int IdProgramador { get; set; }
+
+        private int OrdemExecucao { get; set; }
+
+        private string Descricao { get; set; }
+
+        private DateTime DataPrevistaInicio { get; set; }
+
+        private DateTime DataPrevistaFim { get; set; }
+
+        private int IdTipoTarefa { get; set; }
+
+        private int StoryPoints { get; set; }
+
+        private DateTime DataRealInicio { get; set; }
+        private DateTime DataRealFim { get; set;}
+
+        private DateTime DataCriacao { get; set;}
+
+        private string EstadoAtual { get; set; }
+    }
+}
